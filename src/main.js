5a91f728abbcbc4561dc79e6c528e936e8637e6e
chenhn <chenhn@joinkey.com.cn>
2017-01-23 22:06:24 +0800

    1

--- src/main.js
+++ src/main.js
@@ -656,6 +656,6 @@ function readversion() {
 function fireCallSync() {
   ipcMain.emit("callSync");
 }
-setInterval(fireCallSync, 3 * 60 * 1000);//设置定时器-同步我的盘库，3分钟
+setInterval(fireCallSync, 5 * 60 * 1000);//设置定时器-同步我的盘库，3分钟
 
 setInterval(timerefreshuserinfo, 10 * 60 * 1000);//设置定时器-刷新登录，10分钟
